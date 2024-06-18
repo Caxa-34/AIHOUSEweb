62c2177039a7d3153a94589a3a56b1bd2255b94f
Полина Богатова <polina.bogatova@gmail.com>
2024-06-19 02:40:53 +0300

    11 commit

--- login.js
+++ login.js
@@ -1,49 +1,162 @@
-document.getElementById('registrationForm').addEventListener('submit', async function(event) {
-    event.preventDefault(); // Предотвращаем отправку формы
-   
-    // Собираем данные формы
-    const formData = {
-        name: document.getElementById('tbUsername').value,
-        password: document.getElementById('tbPassword').value,
-        email: document.getElementById('tbEmail').value
-    };
+document.addEventListener('DOMContentLoaded', async function () {
 
-    // Отправляем данные на сервер с помощью fetch
+    // Получить элементы
+    var modal = document.getElementById("rulesModal");
+    var btnShowRules = document.getElementById("btnShowRules");
+    var btnAcceptAndRegister = document.getElementById("btnAcceptAndRegister");
+    var spanClose = document.getElementsByClassName("close")[0];
 
-    try {
+    // Показать модальное окно при нажатии на кнопку "Продолжить"
+    btnShowRules.onclick = async function (event) {
+        modal.style.display = "block";
+
+        const name = document.getElementById('tbUsername').value;
+        const password = document.getElementById('tbPassword').value;
+        const email = document.getElementById('tbEmail').value;
+
+        const formData = {
+            name: name,
+            password: password,
+            email: email
+        };
+
         // Отправляем данные на сервер с помощью fetch
-        const response = await fetch('http://94.228.126.25:3210/api/users/registration', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(formData)
-        });
 
-        // Проверяем статус ответа
-        if (!response.ok) {
-            const errorText = await response.text();
-            console.error('Ошибка:', response.status, errorText);
-            alert('Кажется возникла ошибка. Вы точно не были зарегестрированы ранее: ' + response.status + ' ' + errorText);
-            return;
+        try {
+
+            if (name.value != "" && password.value != "" && email.value != "") {
+
+
+                // Отправляем данные на сервер с помощью fetch
+                const response = await fetch('http://94.228.126.25:3210/api/users/getCode', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(formData)
+                });
+
+                // Проверяем статус ответа
+                if (!response.ok) {
+                    const errorText = await response.text();
+                    console.error('Ошибка:', response.status, errorText);
+                    alert('Кажется возникла ошибка. Ошибка: ' + response.status + ' ' + errorText);
+                    modal.style.display = "none";
+                    return;
+                }
+
+                const data = await response.json();
+                console.log('Ответ от сервера:', data); // Выводим ответ в консоль
+
+                //const varifCode = data.verificationCode;
+                localStorage.setItem('varifCode', data.verificationCode)
+                console.log(localStorage.getItem('varifCode'));
+
+
+            }
+            else {
+                // Если одно из полей пустое, предотвращаем отправку формы
+                event.preventDefault();
+                // Отображаем сообщение б ошибке
+                alert("Упс... кажется вы заполнили не все поля");
+            }
+        } catch (error) {
+            console.error('Ошибка:', error);
+            alert('Ошибка при регистрации');
         }
-
-        const data = await response.json();
-        console.log('Ответ от сервера:', data); // Выводим ответ в консоль
-
-        alert('Пользователь успешно зарегистрирован!');
-        localStorage.setItem('isLoggedIn', 'true');
-        localStorage.setItem('id', data.userData.id);
-        localStorage.setItem('userName', data.userData.name);
-        localStorage.setItem('userImg', data.userData.imagePath);
-        window.location.href="publication.html";
-
-    } catch (error) {
-        console.error('Ошибка:', error);
-        alert('Ошибка при регистрации');
     }
 
+    // Закрыть модальное окно при нажатии на "крестик"
+    spanClose.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // Закрыть модальное окно, если кликнули вне окна
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    // Обработка кнопки "Принять и зарегистрироваться"
+    btnAcceptAndRegister.onclick = async function (event) {
+
+
+
+        const name = document.getElementById('tbUsername').value;
+        const password = document.getElementById('tbPassword').value;
+        const email = document.getElementById('tbEmail').value;
+
+        const formData = {
+            name: name,
+            password: password,
+            email: email
+        };
+
+        // Отправляем данные на сервер с помощью fetch
+
+        if (name.value == "" || password.value == "" || email.value == "") {
+            // Если одно из полей пустое, предотвращаем отправку формы
+            event.preventDefault();
+            // Отображаем сообщение б ошибке
+            alert("Упс... кажется вы заполнили не все поля");
+        }
+        else {
+            try {
+                const varifText = document.getElementById('varificText').value;
+                console.log('Текст из varifText:', varifText);
+
+                const storedValue = localStorage.getItem('varifCode'); // Предполагается, что вы храните значение varifText под этим ключом
+                console.log('Значение из localStorage:', storedValue);
+
+                if (varifText === storedValue) {
+                    console.log('Значения совпадают, продолжаем выполнение запроса.');
+
+                    // Отправляем данные на сервер с помощью fetch
+                    const response = await fetch('http://94.228.126.25:3210/api/users/registration', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(formData)
+                    });
+
+                    // Проверяем статус ответа
+                    if (!response.ok) {
+                        const errorText = await response.text();
+                        console.error('Ошибка:', response.status, errorText);
+                        alert('Кажется возникла ошибка. Вы точно не были зарегестрированы ранее? Ошибка: ' + response.status + ' ' + errorText);
+                        return;
+                    }
+
+                    const data = await response.json();
+                    console.log('Ответ от сервера:', data); // Выводим ответ в консоль
+
+                    alert('Пользователь успешно зарегистрирован!');
+                    localStorage.setItem('isLoggedIn', 'true');
+                    localStorage.setItem('id', data.userData.id);
+                    localStorage.setItem('userName', data.userData.name);
+                    localStorage.setItem('userImg', data.userData.imagePath);
+                    window.location.href = "publication.html";
+                    modal.style.display = "none";
+                }
+                else {
+                    console.error('Значения не совпадают. Окно будет закрыто.');
+                    alert('Значения не совпадают. Регистрация не может быть продолжена.');
+                    document.getElementById('varificText').textContent = "";
+                   // modal.remove; // Закрываем окно
+                }
+            } catch (error) {
+                console.error('Ошибка:', error);
+                alert('Ошибка при регистрации');
+            }
+        }
+        // Закрыть модальное окно
+       // modal.style.display = "none";
+
+    };
+
 });
-    
+
 
 
